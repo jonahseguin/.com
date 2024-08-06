1a8e674153d4075235f12f7e5c462b6a0fa34a43
Jonah Seguin <me@jonahseguin.com>
2024-08-06 16:45:53 -0400

    update resume page

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -3,6 +3,26 @@ import Link from "next/link";
 export default function ResumePage() {
   return (
     <div className="flex flex-col w-full h-full gap-3">
+      <div className="flex flex-row gap-3 w-full">
+        <div className="flex items-center justify-center w-full p-3 border bg-primary text-primary-foreground">
+          <Link
+            href="https://jonahseguin.github.io/resume/resume.pdf"
+            target="_blank"
+            className="font-mono font-bold"
+          >
+            external link to resume
+          </Link>
+        </div>
+        <div className="flex items-center justify-center w-full p-3 border bg-secondary text-secondary-foreground">
+          <Link
+            href="https://github.com/jonahseguin/resume"
+            target="_blank"
+            className="font-mono font-bold"
+          >
+            resume source
+          </Link>
+        </div>
+      </div>
       <object
         data="https://jonahseguin.github.io/resume/resume.pdf"
         type="application/pdf"
@@ -10,16 +30,19 @@ export default function ResumePage() {
         height="100%"
         className="w-full h-full"
       >
-        <p className="text-muted-foreground text-xs font-mono">
-          If my resume is not displaying here,{" "}
-          <Link
-            href="https://jonahseguin.github.io/resume/resume.pdf"
-            target="_blank"
-          >
-            click here
-          </Link>{" "}
-          to view it externally.
-        </p>
+        <div className="flex items-center justify-center w-full h-full border bg-primary p-3 text-primary-foreground">
+          <p className="text-muted-foreground text-xs font-serif font-medium">
+            If my resume is not displaying here,{" "}
+            <Link
+              href="https://jonahseguin.github.io/resume/resume.pdf"
+              target="_blank"
+              className="font-mono font-bold"
+            >
+              click here
+            </Link>{" "}
+            to view it externally.
+          </p>
+        </div>
       </object>
     </div>
   );
