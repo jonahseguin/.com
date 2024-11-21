8074dbf27ea1409cf7f85f9de3e469354bb25bd7
Jonah Seguin <me@jonahseguin.com>
2024-11-20 19:33:51 -0500

    add linkedin to footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,48 +1,57 @@
 import Link from "next/link";
 
 export const Footer = () => {
-  return (
-    <footer className="flex flex-row w-full items-center justify-between gap-6 pb-3">
-      <ul className="flex flex-row gap-3 items-center">
-        <li>
-          <Link
-            href="https://github.com/jonahseguin/.com"
-            target="_blank"
-            className="font-mono text-xs font-normal tracking-tight text-primary lowercase"
-          >
-            source
-          </Link>
-        </li>
-      </ul>
-      <ul className="flex flex-row gap-6 items-center justify-end">
-        <li>
-          <Link
-            href="https://github.com/jonahseguin"
-            target="_blank"
-            className="font-mono text-xs font-normal tracking-tight text-primary lowercase"
-          >
-            github
-          </Link>
-        </li>
-        <li>
-          <Link
-            href="https://instagram.com/jonahseguin"
-            target="_blank"
-            className="font-mono text-xs font-normal tracking-tight text-primary lowercase"
-          >
-            instagram
-          </Link>
-        </li>
-        <li>
-          <Link
-            href="https://x.com/jonahseguin"
-            target="_blank"
-            className="font-mono text-xs font-normal tracking-tight text-primary lowercase"
-          >
-            x
-          </Link>
-        </li>
-      </ul>
-    </footer>
-  );
+	return (
+		<footer className="flex flex-row w-full items-center justify-between gap-6 pb-3">
+			<ul className="flex flex-row gap-3 items-center">
+				<li>
+					<Link
+						href="https://github.com/jonahseguin/.com"
+						target="_blank"
+						className="font-mono text-xs font-normal tracking-tight text-primary lowercase"
+					>
+						source
+					</Link>
+				</li>
+			</ul>
+			<ul className="flex flex-row gap-6 items-center justify-end">
+				<li>
+					<Link
+						href="https://www.linkedin.com/in/jonah-seguin-b53374177/"
+						target="_blank"
+						className="font-mono text-xs font-normal tracking-tight text-primary lowercase"
+					>
+						linkedin
+					</Link>
+				</li>
+				<li>
+					<Link
+						href="https://github.com/jonahseguin"
+						target="_blank"
+						className="font-mono text-xs font-normal tracking-tight text-primary lowercase"
+					>
+						github
+					</Link>
+				</li>
+				<li>
+					<Link
+						href="https://instagram.com/jonahseguin"
+						target="_blank"
+						className="font-mono text-xs font-normal tracking-tight text-primary lowercase"
+					>
+						instagram
+					</Link>
+				</li>
+				<li>
+					<Link
+						href="https://x.com/jonahseguin"
+						target="_blank"
+						className="font-mono text-xs font-normal tracking-tight text-primary lowercase"
+					>
+						x
+					</Link>
+				</li>
+			</ul>
+		</footer>
+	);
 };
